314fb9c6fbddd1649a0bf59ea4360d8300dbdb35
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2018-03-06 11:41:11 +0100

    Test w/ a simple stream constructed from a list

--- test/caffeine/stream_test.exs
+++ test/caffeine/stream_test.exs
@@ -3,115 +3,43 @@ defmodule Caffeine.StreamTest do
   use ExUnitProperties
   doctest Caffeine.Stream
 
-  defmodule Constant.Bound do
-    @moduledoc """
-    A stream of constant values that stops once a limit is reached
-    """
+  defmodule List do
     require Caffeine.Stream
 
-    def stream(limit: 0, value: _) do
+    def stream([]) do
       Caffeine.Stream.sentinel()
     end
 
-    def stream(limit: l, value: v) when is_integer(l) and l > 0 do
-      Caffeine.Stream.construct(v, stream(limit: decrement(l), value: v))
-    end
-
-    defp decrement(x) do
-      x - 1
+    def stream(l) when is_list(l) do
+      Caffeine.Stream.construct(hd(l), stream(tl(l)))
     end
   end
 
-  test "pull elements out" do
-    ## given
-    s = Constant.Bound.stream(limit: 5, value: pi())
-    ## when
-    l = Caffeine.Stream.take(s, 5)
-    ## then
-    assert length(l) === 5
-  end
-
-  test "map/2 changes stream elements" do
-    ## given
-    s = Constant.Bound.stream(limit: 5, value: e())
-    ## when
-    d = Caffeine.Stream.map(s, &double/1)
-    l = Caffeine.Stream.take(d, 5)
-    ## then
-    assert Enum.all?(l, fn x -> 2 * e() === x end)
-  end
-
-  test "reach the end" do
-    ## given
-    s = Constant.Bound.stream(limit: 3, value: pi())
-    ## when
-    l = Caffeine.Stream.take(s, 5)
-    ## then
-    assert length(l) === 3
-  end
-
-  property "take/2 no more than we ask" do
-    check all t <- term(),
-              l <- positive_integer(),
-              i <- positive_integer() do
+  property "head/1 is argument #1 of construct/2" do
+    check all e <- term(),
+              l <- list_of(term()) do
       ## given
-      s = Constant.Bound.stream(limit: l, value: t)
+      r = List.stream(l)
       ## when
-      l = Caffeine.Stream.take(s, i)
+      s = Caffeine.Stream.construct(e, r)
       ## then
-      assert length(l) <= i
+      assert Caffeine.Stream.head(s) == e
     end
   end
 
-  property "take/2 values that we ask" do
-    check all t <- term(),
-              l <- positive_integer(),
-              i <- positive_integer() do
+  property "tail/1 is argument #2 of construct/2" do
+    check all e <- term(),
+              l <- list_of(term()) do
       ## given
-      s = Constant.Bound.stream(limit: l, value: t)
+      r = List.stream(l)
       ## when
-      l = Caffeine.Stream.take(s, i)
+      s = Caffeine.Stream.construct(e, r)
       ## then
-      assert Enum.all?(l, value?(t))
+      assert Caffeine.Stream.tail(s) == List.stream(l)
     end
   end
 
-  property "head/1" do
-    ## given
-    check all t <- term() do
-      ## when
-      s = Caffeine.Stream.construct(t, Caffeine.Stream.sentinel())
-      ## then
-      assert Caffeine.Stream.head(s) === t
-    end
-  end
-
-  property "tail/1" do
-    ## given
-    check all h <- term(),
-              t <- term() do
-      ## when
-      s = Caffeine.Stream.construct(h, t)
-      ## then
-      assert Caffeine.Stream.tail(s) === t
-    end
-  end
-
-  defp pi do
-    3.14159265359
-  end
-
-  defp e do
-    2.71828
-  end
-
-  defp double(x) do
-    2 * x
-  end
-
-  defp value?(x) do
-    fn v ->
-      v == x
-    end
+  defp identity(x) do
+    x
   end
 end
--- test/caffeine_test.exs
+++ test/caffeine_test.exs
@@ -11,5 +11,7 @@ defmodule CaffeineTest do
              tail: 1,
              take: 2
            ]
+
+    assert Caffeine.Stream.__info__(:macros) == [construct: 2]
   end
 end
