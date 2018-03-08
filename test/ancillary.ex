8798e1bf2b0ecd90480aeb4f84a37fde3c7cbdca
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2018-03-08 11:51:35 +0100

    Remove ancillary test module which is no longer used

--- test/ancillary.ex
+++ test/ancillary.ex
@@ -1,23 +1,3 @@
-defmodule CaffeineTest.Ancillary.Natural do
-  require Caffeine.Stream
-
-  def lazy do
-    lazy(0)
-  end
-
-  defp lazy(n) do
-    f = fn ->
-      lazy(increment(n))
-    end
-
-    Caffeine.Stream.construct(n, f)
-  end
-
-  defp increment(n) do
-    n + 1
-  end
-end
-
 defmodule CaffeineTest.Ancillary.List do
   require Caffeine.Stream
 
